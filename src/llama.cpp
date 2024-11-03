303f77307299018ee79ec77b65d435331d2a3818
Diego Devesa <slarengh@gmail.com>
2024-11-03 19:34:08 +0100

    ggml : move CPU backend to a separate file (#10144)

--- src/llama.cpp
+++ src/llama.cpp
@@ -21909,6 +21909,8 @@ int llama_split_prefix(char * dest, size_t maxlen, const char * split_path, int 
 }
 
 const char * llama_print_system_info(void) {
+    ggml_cpu_init(); // some ARM features are detected at runtime
+
     static std::string s;
 
     s  = "";
